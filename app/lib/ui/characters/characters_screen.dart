bfd6ca389e688b755d626ad7e4501e78de606222
KsArT-IT <ksart.it@gmail.com>
2025-02-24 13:45:15 +0200

    feat: Add CharactersScreen with basic counter functionality

---
+++ app/lib/ui/characters/characters_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CharactersScreen extends StatefulWidget {
+  const CharactersScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}
