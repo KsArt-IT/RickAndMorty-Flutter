0ddd4ccd173424f9de6ff0c0d17e5fe29d0719ae
KsArT-IT <ksart.it@gmail.com>
2025-02-25 19:38:31 +0200

    feat: Add CharacterService interface for fetching characters and character details

---
+++ app/lib/data/service/charecter_service.dart
@@ -0,0 +1,7 @@
+import 'package:rick_and_morty/data/model/models.dart';
+import 'package:rick_and_morty/domain/entity/result.dart';
+
+abstract interface class CharacterService {
+  Future<Result<List<CharacterDto>>> fetchCharacters(int page);
+  Future<Result<CharacterDetailDto>> fetchCharacterDetail(int id);
+}
