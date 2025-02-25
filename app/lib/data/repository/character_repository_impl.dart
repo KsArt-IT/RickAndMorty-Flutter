bc0d37b21bbb36c838f681f389897984c9769358
KsArT-IT <ksart.it@gmail.com>
2025-02-25 19:39:13 +0200

    feat: Implement CharacterRepositoryImpl for character data retrieval

---
+++ app/lib/data/repository/character_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:rick_and_morty/data/service/charecter_service.dart';
+import 'package:rick_and_morty/domain/entity/entity.dart';
+import 'package:rick_and_morty/domain/entity/result.dart';
+
+import 'package:rick_and_morty/domain/repository/character_repository.dart';
+
+final class CharacterRepositoryImpl implements CharacterRepository {
+  final CharacterService characterApi;
+
+  CharacterRepositoryImpl({required this.characterApi});
+
+  @override
+  Future<Result<List<Character>>> fetchCharacters(int page) async {
+    final response = await characterApi.fetchCharacters(page);
+    return switch (response) {
+      Success(value: final value) => Result.success(value.map((e) => e.toEntity()).toList()),
+      Failure(error: final error) => Result.failure(error),
+    };
+  }
+  
+  @override
+  Future<Result<CharacterDetail>> fetchCharacterDetail(int id) async {
+    final response = await characterApi.fetchCharacterDetail(id);
+    return switch (response) {
+      Success(value: final value) => Result.success(value.toEntity()),
+      Failure(error: final error) => Result.failure(error),
+    };
+  }
+}
