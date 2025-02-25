df2f6613101c12d9d6730dc3b5dc498e4ad6a1e9
KsArT-IT <ksart.it@gmail.com>
2025-02-25 19:36:32 +0200

    feat: Create models library for character data transfer objects

---
+++ app/lib/data/model/models.dart
@@ -0,0 +1,6 @@
+library models;
+
+export 'package:rick_and_morty/data/model/character/character_dto.dart';
+export 'package:rick_and_morty/data/model/character/character_detail_dto.dart';
+export 'package:rick_and_morty/data/model/character/character_location_dto.dart';
+export 'package:rick_and_morty/data/model/character/character_origin_dto.dart';
