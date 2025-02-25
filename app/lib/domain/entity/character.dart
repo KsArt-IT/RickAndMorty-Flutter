cb4bb1ae26bbe0eb026f9428ceb23f07d5f91b04
KsArT-IT <ksart.it@gmail.com>
2025-02-25 19:37:48 +0200

    feat: Add Character and CharacterDetail domain entities

---
+++ app/lib/domain/entity/character.dart
@@ -0,0 +1,37 @@
+import 'package:equatable/equatable.dart';
+
+final class Character extends Equatable {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  final String locationName;
+  final String locationUrl;
+  final String image;
+  final String url;
+  final String created;
+
+  Character({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+    required this.locationName,
+    required this.locationUrl,
+    required this.image,
+    required this.url,
+    required this.created,
+  });
+
+  @override
+  List<Object?> get props => [
+    id,
+    name,
+    status,
+    locationName,
+  ];
+}
---
+++ app/lib/domain/entity/character_detail.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+
+final class CharacterDetail extends Equatable {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  final String locationName;
+  final String locationUrl;
+  final String originName;
+  final String originUrl;
+  final String image;
+  final String url;
+  final String created;
+  final List<String> episode;
+
+  CharacterDetail({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+    required this.locationName,
+    required this.locationUrl,
+    required this.originName,
+    required this.originUrl,
+    required this.image,
+    required this.url,
+    required this.created,
+    required this.episode,
+  });
+
+  @override
+  List<Object?> get props => [
+    id,
+    name,
+    status,
+    type,
+    locationName,
+  ];
+}
