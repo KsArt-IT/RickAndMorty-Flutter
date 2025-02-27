d1df99e8f4324981a897a68694a8e8f30f925115
KsArT-IT <ksart.it@gmail.com>
2025-02-27 21:29:09 +0200

    refactor: remove unused library declaration in models.dart

--- app/lib/data/model/models.dart
+++ app/lib/data/model/models.dart
@@ -1,5 +1,3 @@
-library models;
-
 export 'package:rick_and_morty/data/model/character/character_dto.dart';
 export 'package:rick_and_morty/data/model/character/character_detail_dto.dart';
 export 'package:rick_and_morty/data/model/character/character_location_dto.dart';
