234d0f9e0defb3bcecf357910f7a1cd11b6856ee
KsArT-IT <ksart.it@gmail.com>
2025-02-28 15:17:54 +0200

    feat: add debug logging in CharacterRepositoryImpl

--- app/lib/data/repository/character_repository_impl.dart
+++ app/lib/data/repository/character_repository_impl.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:rick_and_morty/data/service/charecter_service.dart';
 import 'package:rick_and_morty/domain/entity/entity.dart';
 import 'package:rick_and_morty/domain/entity/result.dart';
-
 import 'package:rick_and_morty/domain/repository/character_repository.dart';
 
 final class CharacterRepositoryImpl implements CharacterRepository {
@@ -11,13 +11,14 @@ final class CharacterRepositoryImpl implements CharacterRepository {
 
   @override
   Future<Result<List<Character>>> fetchCharacters(int page) async {
+    debugPrint('CharacterRepositoryImpl::fetchCharacters: $page');
     final response = await characterApi.fetchCharacters(page);
     return switch (response) {
       Success(value: final value) => Result.success(value.map((e) => e.toEntity()).toList()),
       Failure(error: final error) => Result.failure(error),
     };
   }
-  
+
   @override
   Future<Result<CharacterDetail>> fetchCharacterDetail(int id) async {
     final response = await characterApi.fetchCharacterDetail(id);
