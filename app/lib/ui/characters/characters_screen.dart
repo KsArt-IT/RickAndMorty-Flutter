715b8520a2fcf974e3e626323316271642fea343
KsArT-IT <ksart.it@gmail.com>
2025-03-01 15:21:52 +0200

    feat: implement CharactersScreen with BLoC integration and infinite scrolling

--- app/lib/ui/characters/characters_screen.dart
+++ app/lib/ui/characters/characters_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/di/service_locator.dart';
+import 'package:rick_and_morty/domain/entity/entity.dart';
+import 'package:rick_and_morty/ui/characters/bloc/characters_bloc.dart';
+import 'package:rick_and_morty/ui/characters/widgets/character_list_item.dart';
 
 class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key, required this.title});
@@ -10,37 +17,71 @@ class CharactersScreen extends StatefulWidget {
 }
 
 class _CharactersScreenState extends State<CharactersScreen> {
-  int _counter = 0;
+  late final CharactersBloc _stateBloc;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _stateBloc = getIt<CharactersBloc>()..add(CharactersFetch());
+  }
+
+  @override
+  void dispose() {
+    _stateBloc.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(title: Text(widget.title)),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          final completer = Completer<void>();
+          _stateBloc.add(CharactersFetch(completer: completer));
+          return completer.future;
+        },
+        child: BlocBuilder<CharactersBloc, CharactersState>(
+          bloc: _stateBloc,
+          builder:
+              (context, state) => switch (state) {
+                CharactersLoaded(:final characters, :final hasReachedEnd, :final isLoadingMore) => _buildList(
+                  characters,
+                  hasReachedEnd,
+                  isLoadingMore,
+                ),
+                CharactersError(:final message) => _buildErrorMessage(message),
+                CharactersInitial() => _buildLoadingIndicator(),
+              },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
+  }
+
+  Widget _buildList(List<Character> characters, bool hasReachedEnd, bool isLoadingMore) {
+    debugPrint("_buildList: characters=${characters.length} hasReachedEnd=$hasReachedEnd isLoadingMore=$isLoadingMore");
+    return ListView.builder(
+      itemCount: hasReachedEnd ? characters.length : characters.length + 1,
+      itemBuilder: (context, index) {
+        if (index >= characters.length) {
+          if (!isLoadingMore) {
+            Future.microtask(() {
+              debugPrint("_buildList: CharactersFetchMore");
+              _stateBloc.add(CharactersFetch());
+            });
+          }
+          return _buildLoadingIndicator();
+        }
+        return CharacterListItem(character: characters[index]);
+      },
+    );
+  }
+
+  Widget _buildLoadingIndicator() {
+    return const Padding(padding: EdgeInsets.all(16.0), child: Center(child: CircularProgressIndicator()));
+  }
+
+  Widget _buildErrorMessage(String message) {
+    return Center(child: Text(message));
   }
 }
