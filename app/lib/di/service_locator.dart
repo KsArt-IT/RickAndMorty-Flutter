663db6ac853cd7c77b38ac7b5ab8056070994387
KsArT-IT <ksart.it@gmail.com>
2025-03-01 15:21:13 +0200

    feat: add service locator for dependency injection

---
+++ app/lib/di/service_locator.dart
@@ -0,0 +1,19 @@
+import 'package:get_it/get_it.dart';
+import 'package:rick_and_morty/data/repository/character_repository_impl.dart';
+import 'package:rick_and_morty/data/service/character_service_impl.dart';
+import 'package:rick_and_morty/data/service/charecter_service.dart';
+import 'package:rick_and_morty/domain/repository/character_repository.dart';
+import 'package:rick_and_morty/ui/characters/bloc/characters_bloc.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  // Services
+  getIt.registerLazySingleton<CharacterService>(() => CharacterServiceImpl());
+
+  // Repositories
+  getIt.registerLazySingleton<CharacterRepository>(() => CharacterRepositoryImpl(characterApi: getIt()));
+
+  // BLoCs
+  getIt.registerFactory(() => CharactersBloc(characterRepository: getIt()));
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/app/rick_and_morty_app.dart';
+import 'package:rick_and_morty/di/service_locator.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const RickAndMortyApp());
 }
