c5318d93ba5211d2880c20c775e555a101efc015
KsArT-IT <ksart.it@gmail.com>
2025-03-05 21:12:22 +0200

    feat: update RickAndMortyApp to use routing with AppRoute and initial route setup

--- app/lib/app/rick_and_morty_app.dart
+++ app/lib/app/rick_and_morty_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty/ui/characters/characters_screen.dart';
+import 'package:rick_and_morty/ui/router/router.dart';
 import 'package:rick_and_morty/ui/theme/app_theme.dart';
 
 class RickAndMortyApp extends StatelessWidget {
@@ -12,7 +12,8 @@ class RickAndMortyApp extends StatelessWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const CharactersScreen(title: 'Rick and Morty'),
+      routes: AppRoute.routes,
+      initialRoute: AppRoute.characters.route,
     );
   }
 }
