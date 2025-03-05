3c4f309052d57d318f85db54dbc672eeda22fe43
KsArT-IT <ksart.it@gmail.com>
2025-03-05 21:55:09 +0200

    feat: add onTap callback to CharacterListItem for improved interactivity

--- app/lib/ui/characters/widgets/character_list_item.dart
+++ app/lib/ui/characters/widgets/character_list_item.dart
@@ -3,8 +3,9 @@ import 'package:rick_and_morty/domain/entity/character.dart';
 
 class CharacterListItem extends StatelessWidget {
   final Character character;
+  final Function() onTap;
 
-  const CharacterListItem({super.key, required this.character});
+  const CharacterListItem({super.key, required this.character, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +14,7 @@ class CharacterListItem extends StatelessWidget {
       title: Text(character.name),
       subtitle: Text(character.species),
       trailing: Icon(Icons.arrow_forward_ios),
+      onTap: onTap,
     );
   }
 }
