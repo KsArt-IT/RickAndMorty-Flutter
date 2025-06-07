fb9b6cc9a6de5f670f150df8a66e94e5d942d17e
KsArT-IT <ksart.it@gmail.com>
2025-06-07 20:02:44 +0300

    refactor: update entity import to simplify structure

--- app/lib/ui/characters/bloc/characters_bloc.dart
+++ app/lib/ui/characters/bloc/characters_bloc.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rick_and_morty/domain/entity/entity.dart';
+import 'package:rick_and_morty/domain/entity.dart';
 import 'package:rick_and_morty/domain/entity/result.dart';
 import 'package:rick_and_morty/domain/repository/character_repository.dart';
 
