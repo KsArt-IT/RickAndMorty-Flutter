baf4f4f7ed26610bebb7d45f63bd7beb2690dc8d
KsArT-IT <ksart.it@gmail.com>
2025-06-07 22:49:43 +0300

    refactor: improve import structure in CharacterResponse

--- app/lib/data/model/character/character_response.dart
+++ app/lib/data/model/character/character_response.dart
@@ -1,4 +1,4 @@
-import 'package:rick_and_morty/data/model/character/character_dto.dart';
+import 'package:rick_and_morty/data/models.dart';
 import 'package:rick_and_morty/data/model/character/info.dart';
 
 final class CharacterResponse {
@@ -6,11 +6,19 @@ final class CharacterResponse {
   final Info info;
   final List<CharacterDto> results;
 
-  CharacterResponse({required this.error, required this.info, required this.results});
+  CharacterResponse({
+    required this.error,
+    required this.info,
+    required this.results,
+  });
 
   factory CharacterResponse.fromJson(data) => CharacterResponse(
     error: data['error'],
     info: Info.fromJson(data['info']),
-    results: (data['results'] as List<dynamic>).map((e) => CharacterDto.fromJson(e)).cast<CharacterDto>().toList(),
+    results:
+        (data['results'] as List<dynamic>)
+            .map((e) => CharacterDto.fromJson(e))
+            .cast<CharacterDto>()
+            .toList(),
   );
 }
