36a82c58e96d048f95873c10a791425396c5459a
KsArT-IT <ksart.it@gmail.com>
2025-06-07 20:05:13 +0300

    refactor: remove toEntity method from CharacterDetailDto

--- app/lib/data/model/character/character_detail_dto.dart
+++ app/lib/data/model/character/character_detail_dto.dart
@@ -1,5 +1,4 @@
-import 'package:rick_and_morty/data/model/models.dart';
-import 'package:rick_and_morty/domain/entity/character_detail.dart';
+import 'package:rick_and_morty/data/models.dart';
 
 final class CharacterDetailDto {
   final int id;
@@ -44,23 +43,4 @@ final class CharacterDetailDto {
     type: data['type'],
     url: data['url'],
   );
-
-  CharacterDetail toEntity() {
-    return CharacterDetail(
-      id: id,
-      name: name,
-      image: image,
-      created: created,
-      episode: episode,
-      gender: gender,
-      locationName: location.name,
-      locationUrl: location.url,
-      originName: origin.name,
-      originUrl: origin.url,
-      species: species,
-      status: status,
-      type: type,
-      url: url,
-    );
-  }
 }
