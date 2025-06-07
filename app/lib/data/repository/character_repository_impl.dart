2005716ec98691c66f7973e0a37e51afd9ba2898
KsArT-IT <ksart.it@gmail.com>
2025-06-07 23:13:44 +0300

    refactor: update mapping methods to use toDomain in CharacterRepositoryImpl

--- app/lib/data/repository/character_repository_impl.dart
+++ app/lib/data/repository/character_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty/data/mappers/dto_to_domain.dart';
 import 'package:rick_and_morty/data/service/charecter_service.dart';
-import 'package:rick_and_morty/domain/entity/entity.dart';
+import 'package:rick_and_morty/domain/entity.dart';
 import 'package:rick_and_morty/domain/entity/result.dart';
 import 'package:rick_and_morty/domain/repository/character_repository.dart';
 
@@ -14,7 +15,9 @@ final class CharacterRepositoryImpl implements CharacterRepository {
     debugPrint('CharacterRepositoryImpl::fetchCharacters: $page');
     final response = await characterApi.fetchCharacters(page);
     return switch (response) {
-      Success(value: final value) => Result.success(value.map((e) => e.toEntity()).toList()),
+      Success(value: final value) => Result.success(
+        value.map((e) => e.toDomain()).toList(),
+      ),
       Failure(error: final error) => Result.failure(error),
     };
   }
@@ -23,7 +26,7 @@ final class CharacterRepositoryImpl implements CharacterRepository {
   Future<Result<CharacterDetail>> fetchCharacterDetail(int id) async {
     final response = await characterApi.fetchCharacterDetail(id);
     return switch (response) {
-      Success(value: final value) => Result.success(value.toEntity()),
+      Success(value: final value) => Result.success(value.toDomain()),
       Failure(error: final error) => Result.failure(error),
     };
   }
