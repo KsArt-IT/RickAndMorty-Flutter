0d5370edd9d60f1352774985c3e0d368ff2956e9
KsArT-IT <ksart.it@gmail.com>
2025-06-24 18:08:13 +0300

    feat: add AppColors class to manage application colors

---
+++ app/lib/ui/theme/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const white = Color(0xFFE1F5FE);
+  static const black = Color(0xFF0F0F0F);
+ 
+  static const background = Color(0xFF0064c8);
+  static const backgroundDark = Color(0xFF0064c8);
+  
+  static const Color error = Color(0xFFFF5722);
+  static const Color errorDark = Color(0xFFc80000);
+ 
+  static const blue = Color(0xFF536ef3);
+  static const blueDark = Color(0xFF0000c8);
+  static const c1 = Color(0xFFc8c800);
+  static const c2 = Color(0xFF6400c8);
+  static const c3 = Color(0xFFc800c8);
+
+  static const whiteSmoke = Color(0xFFF8F8F8);
+  static const whiteSmokeDark = Color(0xFFE0E0E0);
+
+  static const gray = Color(0xFF9E9E9E);
+  static const grayDark = Color(0xFF424242);
+}
